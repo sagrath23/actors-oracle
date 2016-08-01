2e649235961ae7d29297d0e6277751f21fbc522d
Steven A. Villegas C <savillegas@open.com.co>
2016-08-01 13:37:12 -0500

    changing styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,8 +29,9 @@
     -->
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.deep_purple-pink.min.css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
     <link rel="stylesheet" href="../styles/styles.css">
     <style>
     #view-source {
@@ -275,7 +276,6 @@
         </footer>
       </main>
     </div>
-    <a href="https://github.com/google/material-design-lite/blob/master/templates/text-only/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">View Source</a>
     <script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
     <script type="text/javascript" src="../js/angularjs/angular.min.js"></script>
     <script type="text/javascript" src="../js/angularjs/angular-resource.min.js"></script>
