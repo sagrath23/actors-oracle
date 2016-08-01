0428e3d2a13ff6c91d212e52be5c89d00a21df90
Steven A. Villegas C <savillegas@open.com.co>
2016-08-01 14:46:15 -0500

    added styles

--- app/Http/Controllers/TMDBController.php
+++ app/Http/Controllers/TMDBController.php
@@ -36,7 +36,7 @@ class TMDBController extends Controller
     }
     
     public function suggestContents(Request $request){
-        return $this->callAPI('search/multi', 'query='.$request->input('data'));
+        return $request->all();//$this->callAPI('search/multi', 'query='.$request->input('data'));
     }
     
     /**
