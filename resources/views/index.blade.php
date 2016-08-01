ef155760e435c738fd96930944e6dcc196aa9efc
Steven A. Villegas C <savillegas@open.com.co>
2016-08-01 13:10:31 -0500

    changing styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,4 @@
 <!doctype html>
-<!--
-  Material Design Lite
-  Copyright 2015 Google Inc. All rights reserved.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License
--->
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -295,37 +279,3 @@
     <script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
   </body>
 </html>
-<?php 
-/*
-<!doctype html>
-<html>
-    <head>
-        <base href="/">
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="An app to fin all about any actor.">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-        <title>The Actor's Oracle</title>
-        <link rel="stylesheet" href="../styles/styles.css">
-    <!-- Angular.js 2-->
-    <!-- 1. Load libraries -->
-    <!-- Polyfill(s) for older browsers -->
-    <script src="../node_modules/core-js/client/shim.min.js"></script>
-    <script src="../node_modules/zone.js/dist/zone.js"></script>
-    <script src="../node_modules/reflect-metadata/Reflect.js"></script>
-    <script src="../node_modules/systemjs/dist/system.src.js"></script>
-    <!-- 2. Configure SystemJS -->
-    <script src="../systemjs.config.js"></script>
-    <script>
-        System.import('app').catch(function (err) {
-            console.error(err);
-        });
-    </script>
-    </head>
-    <!-- 3. Display the application -->
-    <body>
-    <my-app>Loading...</my-app>
-</body>
-</html>
-
-*/
